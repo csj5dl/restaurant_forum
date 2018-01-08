8cf51e6f11c1226d482cbbea7a486d0efc4af824
Jerry Xu <csj5dl@hotmail.com>
2018-01-08 21:16:18 +0800

    admin can delete comment

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -7,6 +7,17 @@ class CommentsController < ApplicationController
     redirect_to restaurant_path(@restaurant)
   end
 
+  def destroy
+    @restaurant = Restaurant.find(params[:restaurant_id])
+    #@comment = @restaurant.comments.find(params[:id])
+    @comment = Comment.find(params[:id])
+    if current_user.admin?
+      @comment.destroy
+      flash[:alter] = "留言已刪除"
+      redirect_to restaurant_path(@restaurant)
+    end
+  end
+
   private
 
   def comment_params
