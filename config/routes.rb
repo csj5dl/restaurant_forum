e83e831112605899ca297e2a6cf5c0294aecbbfc
Jerry Xu <csj5dl@hotmail.com>
2018-01-13 22:51:35 +0800

    add routes for users

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,8 @@ Rails.application.routes.draw do
     resources :comments, only: [:create, :destroy]
   end
   resources :categories, only: :show
+
+  resources :users, only: [:show, :edit, :update]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root "restaurants#index"
 
