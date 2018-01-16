eaf60ee0343ff6f74c086a85c8dc784402b6ca70
Jerry Xu <csj5dl@hotmail.com>
2018-01-16 23:35:46 +0800

    setup relation among user, restaurant and favorite

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -3,4 +3,10 @@ class Restaurant < ApplicationRecord
   validates_presence_of :name
   belongs_to :category
   has_many :comments, dependent: :destroy
+
+  # 「使用者收藏很多餐廳」的多對多關聯
+  # 由於使用 users 會和「使用者評論很多餐廳」重覆，將關聯名稱自訂為 :favorited_users
+  # 自訂名稱後，Rails 無法自動推論來源名稱，需另加 source 告知 model name
+  has_many :favorite, dependent: destroy
+  has_many :favorited_users, through: :favorites, source: :user
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -8,6 +8,12 @@ class User < ApplicationRecord
   #Rails 就會知道 comments table 扮演了 Join Table 的角色
   has_many :restaurants, through: :comments
 
+  # 「使用者收藏很多餐廳」的多對多關聯
+  # 由於使用 restaurants 會和「使用者評論很多餐廳」重覆，將關聯名稱自訂為 :favorited_restaurants
+  # 自訂名稱後，Rails 無法自動推論來源名稱，需另加 source 告知 model name
+  has_many :favorite, dependent: :destroy
+  has_many :favorited_restaurants, through: :favorites, source: :restaurant
+
   mount_uploader :avatar, PhotoUploader
   validates_presence_of :name
 
